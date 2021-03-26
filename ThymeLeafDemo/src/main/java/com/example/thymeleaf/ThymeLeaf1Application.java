b2e036e4f843cf70ac0e601b66619dfba80999ab
nikhilbindal <nikhilbindal9654@gmail.com>
2021-03-26 23:50:57 +0530

    demo project

---
+++ ThymeLeafDemo/src/main/java/com/example/thymeleaf/ThymeLeaf1Application.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeaf1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeaf1Application.class, args);
+	}
+
+}
